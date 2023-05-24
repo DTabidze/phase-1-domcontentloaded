d55e337cb6e50e16ee47d828593148d928d6e81f
Davit Tabidze <davidtabidzef1@gmail.com>
2023-05-24 09:56:11 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    console.log ("DOM has been Loaded");
+    const newText = document.querySelector("#text");
+    newText.textContent = "This is really cool!";
+    //newText.appendChild
+})
